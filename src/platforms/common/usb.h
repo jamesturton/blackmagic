3564d9984ee6b7d19792493a5593117a9feff651
Uwe Bonnes <bon@elektron.ikp.physik.tu-darmstadt.de>
2021-11-30 14:40:46 +0100

    stlinkv3: Try to handle CDCACM_PACKET_SIZE
    Will probably not enumerate on a FS host.

--- src/platforms/common/usb.h
+++ src/platforms/common/usb.h
@@ -27,7 +27,9 @@
 extern usbd_device *usbdev;
 extern uint16_t usb_config;
 
-#if !defined(CDCACM_PACKET_SIZE)
+#if defined(USB_HS)
+# define CDCACM_PACKET_SIZE 512
+#else
 # define CDCACM_PACKET_SIZE 64
 #endif
 
