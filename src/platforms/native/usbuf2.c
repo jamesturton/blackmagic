80ddf28ebc2347ca5cc3e00a63a2362e3d0657a3
James Turton <james.turton@gmx.com>
2022-10-06 15:06:14 +0200

    uf2: Added uf2 bootloader for native

---
+++ src/platforms/native/usbuf2.c
@@ -0,0 +1,151 @@
+/*
+ * Copyright (c) 2016, Devan Lai
+ *
+ * Permission to use, copy, modify, and/or distribute this software
+ * for any purpose with or without fee is hereby granted, provided
+ * that the above copyright notice and this permission notice
+ * appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+ * WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+ * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+ * NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/* Common STM32F103 target functions */
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/st_usbfs.h>
+#include <libopencm3/stm32/flash.h>
+#include <libopencm3/stm32/desig.h>
+#include <libopencm3/cm3/scb.h>
+#include <libopencm3/usb/usbd.h>
+
+#include "target.h"
+#include "platform.h"
+#include "uf2_config.h"
+
+
+// static const uint32_t CMD_BOOT = 0x544F4F42UL;
+// static const uint32_t CMD_APP = 0x3f82722aUL;
+
+//#define USE_HSI 1
+
+void target_clock_setup(void) {
+    rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE8_72MHZ]);
+}
+
+void target_set_led(int on) {
+    if (on)
+        gpio_clear(LED_PORT, LED_1);
+    else
+        gpio_set(LED_PORT, LED_1);
+}
+
+void target_gpio_setup(void) {
+    /* Enable GPIO clocks */
+    rcc_periph_clock_enable(RCC_GPIOA);
+    rcc_periph_clock_enable(RCC_GPIOB);
+    rcc_periph_clock_enable(RCC_GPIOC);
+
+    /* Configure the LED pins. */
+    gpio_set_mode(LED_PORT, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, LED_0 | LED_1 | LED_2);
+
+	/* Configure the USB pull up pin. */
+	gpio_set(USB_PU_PORT, USB_PU_PIN);
+	gpio_set_mode(USB_PU_PORT, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, USB_PU_PIN);
+}
+
+const usbd_driver* target_usb_init(void) {
+    rcc_periph_reset_pulse(RST_USB);
+
+    /* Enable USB pullup to connect */
+    gpio_set(USB_PU_PORT, USB_PU_PIN);
+
+    return &st_usbfs_v1_usb_driver;
+}
+
+bool target_get_force_bootloader(void) {
+    /* Check if the user button is held down */
+    rcc_periph_clock_enable(RCC_GPIOB);
+    if (gpio_get(AUX_BTN1_PORT, AUX_BTN1))
+        return false;
+
+    return true;
+}
+
+void target_get_serial_number(char* dest, size_t max_chars) {
+    desig_get_unique_id_as_string(dest, max_chars+1);
+}
+
+static uint16_t* get_flash_end(void) {
+#ifdef FLASH_SIZE_OVERRIDE
+    /* Allow access to the unofficial full 128KiB flash size */
+    return (uint16_t*)(FLASH_BASE + FLASH_SIZE_OVERRIDE);
+#else
+    /* Only allow access to the chip's self-reported flash size */
+    return (uint16_t*)(FLASH_BASE + desig_get_flash_size());
+#endif
+}
+
+size_t target_get_max_firmware_size(void) {
+    uint8_t* flash_end = (uint8_t*)get_flash_end();
+    uint8_t* flash_start = (uint8_t*)(APP_BASE_ADDRESS);
+
+    return (flash_end >= flash_start) ? (size_t)(flash_end - flash_start) : 0;
+}
+
+void target_relocate_vector_table(void) {
+    SCB_VTOR = APP_BASE_ADDRESS & 0xFFFF;
+}
+
+void target_flash_unlock(void) {
+    flash_unlock();
+}
+
+void target_flash_lock(void) {
+    flash_lock();
+}
+
+static inline uint16_t* get_flash_page_address(uint16_t* dest) {
+    return (uint16_t*)(((uint32_t)dest / FLASH_PAGE_SIZE) * FLASH_PAGE_SIZE);
+}
+
+bool target_flash_program_array(uint16_t* dest, const uint16_t* data, size_t half_word_count) {
+    bool verified = true;
+
+    /* Remember the bounds of erased data in the current page */
+    static uint16_t* erase_start;
+    static uint16_t* erase_end;
+
+    const uint16_t* flash_end = get_flash_end();
+    while (half_word_count > 0) {
+        /* Avoid writing past the end of flash */
+        if (dest >= flash_end) {
+            verified = false;
+            break;
+        }
+
+        if (dest >= erase_end || dest < erase_start) {
+            erase_start = get_flash_page_address(dest);
+            erase_end = erase_start + (FLASH_PAGE_SIZE)/sizeof(uint16_t);
+            flash_erase_page((uint32_t)erase_start);
+        }
+        flash_program_half_word((uint32_t)dest, *data);
+        erase_start = dest + 1;
+        if (*dest != *data) {
+            verified = false;
+            break;
+        }
+        dest++;
+        data++;
+        half_word_count--;
+    }
+
+    return verified;
+}
